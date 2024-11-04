af8ad4bcb657ebd3a61b1a6227cd113c42581c39
Dana Basken <dana@bainbridgegrowth.com>
2024-11-04 11:58:47 -0500

    created ExcelFarm to manage multiple Excel processes, cleanup

---
+++ ExcelFarm.py
@@ -0,0 +1,66 @@
+#
+# Copyright ©2024 Dana Basken
+#
+
+import asyncio
+import multiprocessing
+import win32com.client as win32
+import pythoncom
+import uuid
+
+class ExcelFarm:
+
+    def __init__(self, worker_count: int = 5):
+        self._workers = []
+        self._requests = multiprocessing.Queue()
+        self._responses = multiprocessing.Queue()
+        self._start_workers(worker_count)
+        asyncio.create_task(self._response_handler(self._responses))
+
+    def add_task(self, workbook_path: str) -> str:
+        id = str(uuid.uuid4())
+        self._requests.put({"id": id, "workbook_path": workbook_path})
+        return id
+
+    def _start_workers(self, worker_count: int):
+        for index in range(worker_count):
+            worker_process = multiprocessing.Process(target=self._worker, args=(self._requests, self._responses))
+            worker_process.start()
+            self._workers.append(worker_process)
+
+    @staticmethod
+    async def _response_handler(responses):
+        loop = asyncio.get_event_loop()
+        while True:
+            result = await loop.run_in_executor(None, responses.get)
+            print(result)
+
+    @staticmethod
+    def _worker(requests: multiprocessing.Queue, responses: multiprocessing.Queue):
+        try:
+            pythoncom.CoInitialize()
+            excel = win32.DispatchEx("Excel.Application")
+            excel.Visible = False
+            while True:
+                try:
+                    task = requests.get()
+                    print("task", task)
+                    if task == "STOP": break
+                    task_id = task["id"]
+                    workbook_path = task["workbook_path"]
+                    print("opening workbook", workbook_path)
+                    workbook = excel.Workbooks.Open(workbook_path)
+                    # TODO: something
+                    print("closing workbook", workbook_path)
+                    workbook.Close(SaveChanges=False)
+                    responses.put({"id": task_id, "result": {}})
+                    print("task complete", task)
+                except Exception as exception:
+                    print("error", exception)
+                    responses.put({"id": task_id, "error": str(exception)})
+            print("quitting excel")
+            excel.Quit()
+        except Exception as exception:
+            print(exception)
+        finally:
+            pythoncom.CoUninitialize()
--- main.py
+++ main.py
@@ -2,31 +2,18 @@
 # Copyright ©2024 Dana Basken
 #
 
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from config import settings
-from services.ExcelService import shutdown_excel_service, start_excel_service
-from controllers import ChatGPTController, TestController, DriveItemController, GroupController
+from ExcelFarm import ExcelFarm
+from controllers import TestController, DriveItemController, GroupController
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     print("lifespan startup")
-#     start_excel_service()
-#     yield
-#     print("lifespan shutdown")
-#     shutdown_excel_service()
+farm = ExcelFarm(10)
 
 app = FastAPI(title="Drivepoint Raptor Service", version=settings.get("version"))
 
-app.include_router(ChatGPTController.router)
 app.include_router(DriveItemController.router)
 app.include_router(GroupController.router)
 app.include_router(TestController.router)
 
-@app.on_event("startup")
-def startup_event():
-    start_excel_service()
-
-@app.on_event("shutdown")
-def shutdown_event():
-    shutdown_excel_service()
+farm.add_task(r"C:\Users\dana\raptor-service\test.xlsx")
+farm.add_task(r"C:\Users\dana\raptor-service\test2.xlsx")
